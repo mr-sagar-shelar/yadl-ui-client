b7700c3f2f303658ce73b79974783b37d8fa126e
mr-sagar-shelar <shaggy.shelar@gmail.com>
2025-03-28 12:25:17 +0530

    Minor change

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import "./animate.min.css";
+import { Button, AWS } from "yadl-ui";
 
 function App() {
   return (
     <div>
       <h1 class="text-3xl font-bold underline">Hello world 12!</h1>
+      <Button />
+      <AWS />
     </div>
   );
 }
