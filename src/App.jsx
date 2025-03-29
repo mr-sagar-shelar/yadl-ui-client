7bc7e169cfd7ade5be16f1c97f9fb5302dff2b27
mr-sagar-shelar <shaggy.shelar@gmail.com>
2025-03-29 10:55:14 +0530

    Added preview component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import "./App.css";
 import "./animate.min.css";
-import { Button, AWS } from "yadl-ui";
+import "@xyflow/react/dist/style.css";
+import { Button, AWS, GCPCertificateManager, Preview } from "yadl-ui";
 
 function App() {
   return (
     <div>
-      <h1 class="text-3xl font-bold underline">Hello world 12!</h1>
+      {/* <h1 class="text-3xl font-bold underline">Hello world 12!</h1>
       <Button />
       <AWS />
+      <GCPCertificateManager width={100} height={100} /> */}
+      <div style={{ height: "90vh", width: "100%" }}>
+        <Preview />
+      </div>
     </div>
   );
 }
