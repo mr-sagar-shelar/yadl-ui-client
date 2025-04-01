c923edc8fe5625d0646117ebd5c6616d48047af1
mr-sagar-shelar <shaggy.shelar@gmail.com>
2025-04-01 11:08:53 +0530

    Added preview component with nodes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,55 @@
 import "./App.css";
 import "./animate.min.css";
 import "@xyflow/react/dist/style.css";
-import { Button, AWS, GCPCertificateManager, Preview } from "yadl-ui";
+import "yadl-ui/dist/yadl-ui.css";
+import { Button, AWS, GCPCertificateManager, YadlPreview } from "yadl-ui";
 
 function App() {
+  const nodes = [
+    {
+      id: "1",
+      data: { label: "Hello" },
+      position: { x: 0, y: 0 },
+      type: "input",
+    },
+    {
+      id: "2",
+      data: { label: "World" },
+      position: { x: 100, y: 100 },
+    },
+    {
+      id: "icon-0",
+      position: {
+        x: 0,
+        y: 110,
+      },
+      data: {
+        icon: "athena",
+        width: 50,
+        height: 50,
+      },
+      type: "icon",
+    },
+  ];
+  const edges = [
+    { id: "1-2", source: "1", target: "2", label: "to the", type: "step" },
+    { id: "1-icon-0", source: "1", target: "icon-0", type: "step" },
+  ];
   return (
     <div>
       {/* <h1 class="text-3xl font-bold underline">Hello world 12!</h1>
       <Button />
       <AWS />
       <GCPCertificateManager width={100} height={100} /> */}
-      <div style={{ height: "90vh", width: "100%" }}>
-        <Preview />
+      {/* <div style={{ height: "100%", width: "100%" }}>
+        <div className="dndflow">
+          <div className="reactflow-wrapper">
+            <YadlPreview nodes={nodes} edges={edges} />
+          </div>
+        </div>
+      </div> */}
+      <div style={{ height: "100vh", width: "100%" }}>
+        <YadlPreview nodes={nodes} edges={edges} />
       </div>
     </div>
   );
