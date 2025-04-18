baebcba1ad5ba0b718ef7d4702b74d8eb058d508
mr-sagar-shelar <shaggy.shelar@gmail.com>
2025-04-18 17:42:10 +0530

    Added base URL for github page deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "yadl-ui-client",
   plugins: [react(), tailwindcss()],
   optimizeDeps: { exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"] },
   server: {
